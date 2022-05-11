50abb4cd76c0484b135f20d97fa7d1778cfd4bfc
par4m <param2k3@gmail.com>
2022-05-11 14:07:04 +0530

    impatient profile

--- init.lua
+++ init.lua
@@ -1,22 +1,21 @@
--- to be run first --
-local ok, impatient = pcall(require, 'impatient')
-if not ok then
-  vim.notify(impatient)
-  return
+local present, impatient = pcall(require, "impatient")
+
+if present then
+	impatient.enable_profile()
 end
 
 -- General Settings --
-require "user.options"
-require "user.keymaps"
-require "user.plugins" -- manage plugins
+require("user.options")
+require("user.keymaps")
+require("user.plugins") -- manage plugins
 
 -- Theme --
-require "themes.gruvbox-flat" -- change theme
+require("themes.gruvbox-flat") -- change theme
 -- require "user.colorscheme" -- colorscheme protected call
 
 -- LSP and Completion --
-require "user.cmp" -- completion settings for nvim-cmp
-require "user.lsp" --include lsp directory under user, init.lua inside it will be run first
+require("user.cmp") -- completion settings for nvim-cmp
+require("user.lsp") --include lsp directory under user, init.lua inside it will be run first
 
 -- Plugin Configs --
 -- require "cfg.telescope" -- fuzzy finder
